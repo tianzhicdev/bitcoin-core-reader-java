38fb0e76b728ce946d6f15bcebe529ad4f8972e0
SilentWatch <burgesschen1990@gmail.com>
2025-02-14 13:54:07 -0500

    balance

--- src/main/java/org/bitcoin/reader/BalanceProcessor.java
+++ src/main/java/org/bitcoin/reader/BalanceProcessor.java
@@ -17,11 +17,24 @@ public class BalanceProcessor extends AbstractRWProcessor<BalanceRecord> {
     }
 
     @Override
+    protected int getHighestBlockNumber(Connection conn, String tableName) throws SQLException {
+        return 678901;
+    }
+
+    @Override
     protected List<BalanceRecord> read(Connection conn, int fromBlockNumber, int toBlockNumber) throws Exception {
         List<BalanceRecord> allBalanceRecords = new ArrayList<>();
         for (int blockNumber = fromBlockNumber; blockNumber < toBlockNumber; blockNumber++) {
             try {
-                List<Transaction> transactions = Utils.getTransactions(conn, blockNumber);
+                List<Transaction> transactions;
+                while (true) {
+                    transactions = Utils.getTransactions(conn, blockNumber);
+                    if (!transactions.isEmpty()) {
+                        break;
+                    }
+                    logger.warn("No transactions found for block " + blockNumber + ". Retrying in 1 minute.");
+                    Thread.sleep(60000); // Wait for 1 minute before retrying
+                }
                 for (Transaction transaction : transactions) {
                     try {
                         List<BalanceRecord> balanceRecords = Utils.getBalanceRecords(conn, transaction, blockNumber, logger);
@@ -41,6 +54,9 @@ public class BalanceProcessor extends AbstractRWProcessor<BalanceRecord> {
             } catch (SQLException e) {
                 logger.error("Error reading balance records for block " + blockNumber + ": ", e);
                 throw e;
+            } catch (InterruptedException e) {
+                logger.error("Thread interrupted while waiting to retry: ", e);
+                Thread.currentThread().interrupt(); // Restore interrupted status
             }
         }
         return allBalanceRecords;
@@ -67,7 +83,7 @@ public class BalanceProcessor extends AbstractRWProcessor<BalanceRecord> {
         int readerThreads = args.length > 0 ? Integer.parseInt(args[0]) : 1; // Number of BlockReader threads
         int writerThreads = args.length > 1 ? Integer.parseInt(args[1]) : 1;  // Number of DBWriter threads
         int queueSize = args.length > 2 ? Integer.parseInt(args[2]) : 100; // Queue size for transactionQueue
-        int readBatchSize = args.length > 3 ? Integer.parseInt(args[3]) : 5; // Read batch size for BlockReader
+        int readBatchSize = args.length > 3 ? Integer.parseInt(args[3]) : 1; // Read batch size for BlockReader
         int minBatchSize = args.length > 4 ? Integer.parseInt(args[4]) : 20; // Smallest size for DBWriter
         int maxBatchSize = args.length > 5 ? Integer.parseInt(args[5]) : 10; // Max batch size for DBWriter
 
