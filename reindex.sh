7000b0e6b2abd8ae3a1e23920a71d8043d54cea4
SilentWatch <burgesschen1990@gmail.com>
2025-02-23 13:22:15 -0500

    reindex

--- reindex.sh
+++ reindex.sh
@@ -7,21 +7,25 @@ DB_NAME="bitcoin"
 DB_USER="abc"
 DB_PASSWORD="12345"
 
-# Function to reindex a given index
-reindex_index() {
+# Function to reindex a given index with no timeout
+reindex_index_no_timeout() {
   local index_name=$1
-  PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c "REINDEX INDEX $index_name;" &
+  PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d $DB_NAME -c "
+  BEGIN;
+  SET statement_timeout = 0;
+  REINDEX INDEX $index_name;
+  COMMIT;" &
 }
 
-# Reindex all indices in parallel
-reindex_index "idx_unprocessed_transactions_for_balance_txid"
-reindex_index "idx_unprocessed_transactions_for_balance_block_number"
-reindex_index "idx_balance_java_address"
-reindex_index "idx_balance_java_txid"
-reindex_index "idx_balance_java_block_number"
-reindex_index "idx_transactions_java_indexed_txid"
-reindex_index "idx_transactions_java_indexed_block_number"
-reindex_index "idx_transactions_java_indexed_unique"
+# Reindex all indices in parallel with no timeout
+reindex_index_no_timeout "idx_unprocessed_transactions_for_balance_txid"
+reindex_index_no_timeout "idx_unprocessed_transactions_for_balance_block_number"
+reindex_index_no_timeout "idx_balance_java_address"
+reindex_index_no_timeout "idx_balance_java_txid"
+reindex_index_no_timeout "idx_balance_java_block_number"
+reindex_index_no_timeout "idx_transactions_java_indexed_txid"
+reindex_index_no_timeout "idx_transactions_java_indexed_block_number"
+reindex_index_no_timeout "idx_transactions_java_indexed_unique"
 
 # Wait for all background processes to complete
 wait
